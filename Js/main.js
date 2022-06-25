c4adaa1b0a1c7a35f8badb915944d5e6fcfb6f7c
RouxGit <DRoux.email@gmail.com>
2022-06-25 11:19:23 -0300

    Add work, projects and experience section
    and fixed bugs

--- Js/main.js
+++ Js/main.js
@@ -17,7 +17,7 @@
 
 /*=============== HOME SWIPER ===============*/
 
-var homeSwiper = new Swiper(".home-swiper", {
+var homeSwiper = new Swiper('.home-swiper', {
     spaceBetween: 16,
     centeredSlides: true,
     slidesPerView: "auto",
@@ -25,6 +25,27 @@ var homeSwiper = new Swiper(".home-swiper", {
 });
 
 /*=============== SERVICES TAB ===============*/
+
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContent = document.querySelectorAll('[data-content]')
+    
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContent.forEach(tabContents => {
+      tabContents.classList.remove('services__active')
+    })
+
+    target.classList.add('services__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('services__active')
+    })
+
+    tab.classList.add('services__active')
+  })
+})
 
 
 /*=============== LIGHTBOX GALLERY ===============*/
